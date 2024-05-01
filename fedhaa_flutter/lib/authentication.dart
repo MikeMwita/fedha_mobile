a2f13192c351e5def3f755dc2de0c3c7fc1ee056
MikeMwita <mwitamike8@gmail.com>
2024-05-01 16:34:03 +0300

    Setup auth feature

---
+++ fedhaa_flutter/lib/authentication.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+
+class LocalAuthApi {
+  static final _auth = LocalAuthentication();
+
+  static Future<bool> hasBiometrics() async {
+    try {
+      return await _auth.canCheckBiometrics;
+    } on PlatformException catch (_) {
+      return false;
+    }
+  }
+
+  static Future<bool> authenticate() async {
+    final isAvailable = await hasBiometrics();
+    if (!isAvailable) return false;
+
+    try {
+      return await _auth.authenticate(
+        localizedReason: 'Scan Face to Authenticate',
+        options: const AuthenticationOptions(
+          useErrorDialogs: true,
+          stickyAuth: true,
+        ),
+      );
+    } on PlatformException catch (_) {
+      return false;
+    }
+  }
+}
